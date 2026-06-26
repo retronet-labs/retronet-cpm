c6bde0fbf749d6b2d8f34702b4c01519f886b1f9
Andrea Solimine <andreasolimine@gmail.com>
2026-06-26 15:25:40 +0200

    docs(examples): use logical COM origin in examples

--- examples/hello-bdos.asm
+++ examples/hello-bdos.asm
@@ -1,8 +1,9 @@
 .arch i8080
+.com
 
-        LXI D, 0x010D    ; indirizzo runtime del messaggio nel .COM caricato a 0100h
+        LXI D, msg
         MVI C, 9         ; BDOS print string, terminata da '$'
         CALL 0x0005
         MVI C, 0         ; BDOS terminate
         CALL 0x0005
-        .byte 0x48, 0x49, 0x24
+msg:    .byte 0x48, 0x49, 0x24
--- examples/type-dollar.asm
+++ examples/type-dollar.asm
@@ -1,24 +1,20 @@
 .arch i8080
+.com
 
 ; TYPE-like minimale: apre DOLLAR.TXT dal drive A:, legge un record nel DMA
 ; 0200h e lo stampa come stringa BDOS 9. Il file deve contenere un '$' nel primo
 ; record, per esempio: CIAO DA A:$.
-;
-; Indirizzi calcolati per caricamento .COM a 0100h:
-;   FCB = 0125h
-;   DMA = 0200h
 
 .equ BDOS 0x0005
-.equ FCB 0x0125
 .equ DMA 0x0200
 
-        LXI D, FCB
+        LXI D, fcb
         MVI C, 15
         CALL BDOS
         LXI D, DMA
         MVI C, 26
         CALL BDOS
-        LXI D, FCB
+        LXI D, fcb
         MVI C, 20
         CALL BDOS
         LXI D, DMA
@@ -27,7 +23,7 @@
         MVI C, 0
         CALL BDOS
 
-        .byte 0x00
+fcb:    .byte 0x00
         .byte 0x44, 0x4F, 0x4C, 0x4C, 0x41, 0x52, 0x20, 0x20
         .byte 0x54, 0x58, 0x54
         .byte 0x00, 0x00, 0x00, 0x00
